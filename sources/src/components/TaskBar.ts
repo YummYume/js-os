3aa2f151e811e6b02bd29fefd85c5b0f2234314c
YummYume <givsoul@gmail.com>
2022-10-21 23:15:59 +0200

    Added an overall layout and improved the TaskBar component

--- sources/src/components/TaskBar.ts
+++ sources/src/components/TaskBar.ts
@@ -2,6 +2,16 @@ import htmlTemplate from '@templates/TaskBar.html?raw';
 import CustomElement from '@/CustomElement';
 
 class TaskBar extends CustomElement {
+    #currentTime = new Date();
+    #currentTimeCallback: number | undefined;
+    #intlTimeFormat = new Intl.DateTimeFormat(
+        'fr',
+        { hour: 'numeric', minute: 'numeric', hour12: false }
+    );
+    #intlDateFormat = new Intl.DateTimeFormat(
+        'fr',
+        { day: '2-digit', month: 'long', year: 'numeric' }
+    );
     items = [];
 
     #isArrayValue = [
@@ -19,20 +29,49 @@ class TaskBar extends CustomElement {
         }
 
         const template = this.getTemplate();
-        const container = template.querySelector('#container');
+        const tasksContainer = template.querySelector('#tasks');
+        const infoContainer = template.querySelector('#info');
 
-        if (!container) {
-            return;
+        if (tasksContainer) {
+            this.items.forEach((item) => {
+                const navItem = document.createElement('span');
+                navItem.textContent = item;
+
+                tasksContainer.appendChild(navItem);
+            });
         }
 
-        this.items.forEach((item) => {
-            const navItem = document.createElement('span');
-            navItem.textContent = item;
+        const currentTimeSpan = infoContainer?.querySelector('#current-time');
+        const currentDateSpan = infoContainer?.querySelector('#current-date');
 
-            container.appendChild(navItem);
-        });
+        if (currentTimeSpan) {
+            currentTimeSpan.textContent = this.#intlTimeFormat.format(this.#currentTime);
+        }
+
+        if (currentDateSpan) {
+            currentDateSpan.textContent = this.#intlDateFormat.format(this.#currentTime);
+        }
+
+        if (currentTimeSpan) {
+            this.#currentTimeCallback = setInterval(() => {
+                this.#currentTime = new Date();
+
+                if (currentTimeSpan) {
+                    currentTimeSpan.textContent = this.#intlTimeFormat.format(this.#currentTime);
+                }
+
+                // New day
+                if (currentDateSpan &&this.#currentTime.getHours() === 0 && this.#currentTime.getMinutes() === 0 && this.#currentTime.getSeconds() === 0) {
+                    currentDateSpan.textContent = this.#intlDateFormat.format(this.#currentTime);
+                }
+            }, 1000);
+        }
 
         this.appendToShadow(template);
+    }
+
+    disconnectedCallback() {
+        clearInterval(this.#currentTimeCallback);
     }
 
     static get observedAttributes() {
